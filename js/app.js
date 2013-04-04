fd2661b2e454bf0fbb76ad2a4d1c1c1553c43bd8
harry patel <harry@unomee.com>
2013-04-04 14:15:01 +0100

    completed adding and deletion of books.

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@
         tagName: "div",
         className: "bookContainer",
         template: $("#bookTemplate").html(),
+        events: {"click .delete": "deleteBook"},
 
         render: function () {
             var tmpl = _.template(this.template);
@@ -20,6 +21,12 @@
             this.$el.html(tmpl(this.model.toJSON()));
 
             return this;
+        },
+
+        deleteBook: function () {
+            this.model.destroy();
+
+            this.remove();
         }
     });
 
@@ -38,15 +45,20 @@
     var LibraryView = Backbone.View.extend({
         el: $("#books"),
 
+        events: {"click #add": "addBook"},
+
         initialize: function () {
             this.collection = new Library(books);
             this.render();
+
+            this.collection.on("add", this.renderBook, this);
+            this.collection.on("remove", this.removeBook, this);
         },
 
         render: function () {
-            var that = this;
+            var self = this;
             _.each(this.collection.models, function (item) {
-                that.renderBook(item);
+                self.renderBook(item);
             }, this);
         },
 
@@ -55,6 +67,40 @@
                 model: item
             });
             this.$el.append(bookView.render().el);
+        },
+
+        removeBook: function (bookRemoved) {
+            var removedBookData = bookRemoved.attributes;
+
+            // first remove the attributes that have not changed from their defaults
+            _.each(removedBookData, function(val, key){
+                if (removedBookData[key] === bookRemoved.defaults[key]) {
+                    delete removedBookData[key];
+                }
+            });
+
+            _.each(books, function(book){
+               if (_.isEqual(book, removedBookData)) {
+                   books.splice(_.indexOf(books, book), 1);
+               }
+            });
+        },
+
+        addBook: function (e) {
+            e.preventDefault();
+
+            var formData = {};
+
+            $("#addBook div").children("input").each(function (i, el) {
+                var val = $(el).val();
+                if (val !== "") {
+                    formData[el.id] = val;
+                }
+            });
+
+            books.push(formData);
+
+            this.collection.add(new Book(formData));
         }
     });
 
